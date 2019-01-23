4a7bb9668ed8d0ba4d14d1c067b05b085656264b
Yusuf <yusufkinatas@gmail.com>
2019-01-23 18:59:06 +0300

    Calculation speed time is increased to 30 from 20. Now answers are always positive numbers

--- src/ui/screens/Games/CalculationSpeedGame.js
+++ src/ui/screens/Games/CalculationSpeedGame.js
@@ -21,7 +21,7 @@ import CustomButton from "../../../components/CustomButton";
 import SwappingText from "../../../components/SwappingText";
 import BouncingText from "../../../components/BouncingText";
 
-const TIMEOUT_MS = 20000;
+const TIMEOUT_MS = 30000;
 const QUESTION_FUNCTIONS = [
   (x, y, z) => ({
     text: `${x} + ${y} - ${z} = ?`,
@@ -103,14 +103,17 @@ export default class CalculationSpeedGame extends Component {
 
   showNewQuestion = () => {
     let x, y, z;
-    x = utils.randomBetween(1, 20);
-    y = utils.randomBetween(1, 20);
-    z = utils.randomBetween(1, 20);
-    let randomQuestion = QUESTION_FUNCTIONS[Math.floor(Math.random() * QUESTION_FUNCTIONS.length)](x, y, z);
-    if (this.textInputRef) {
-      this.textInputRef.clear();
-    }
-    this.trueAnswer = randomQuestion.answer;
+    let randomQuestion;
+    do {
+      x = utils.randomBetween(1, 20);
+      y = utils.randomBetween(1, 20);
+      z = utils.randomBetween(1, 20);
+      randomQuestion = QUESTION_FUNCTIONS[Math.floor(Math.random() * QUESTION_FUNCTIONS.length)](x, y, z);
+      if (this.textInputRef) {
+        this.textInputRef.clear();
+      }
+      this.trueAnswer = randomQuestion.answer;
+    } while (this.trueAnswer < 0);
     this.setState({ question: randomQuestion.text });
   }
 
